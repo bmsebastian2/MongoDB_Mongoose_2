2569ada35f7ff94b75f331075e742eaf98a8bfad
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-08-17 20:48:02 -0300

    int3

--- myApp.js
+++ myApp.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .catch((e) => console.log("err:" + e));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((e) => console.log("err:" + e));
 
 let Person;
 
