0fbbaad5a52b6dc20d9a8d9245a841ed7901c8ba
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-08-17 20:03:32 -0300

    configurar Mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((e) => console.log("err:" + e));
 
 let Person;
 
