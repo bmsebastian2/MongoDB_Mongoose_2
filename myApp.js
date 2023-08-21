e8a295b2b4473cf9bf43aad6e5c222399e786d67
Replit user <>
2023-08-21 16:13:21 +0000

    12.3

--- myApp.js
+++ myApp.js
@@ -107,11 +107,15 @@ const removeById = (personId, done) => {
 };
 
 const removeManyPeople = (done) => {
-  const nameToRemove = "Mary";
-  Person.romove({ name: nameToRemove }, (err, dataJson) => {
-    if (err) return console.log(err)
-    done(null, dataJson);
-  })
+
+   const nameToRemove = "Mary";
+  Person.remove(
+    {name: nameToRemove},
+    (err, data) => {
+      if(err) return console.log(err);
+      done(null, data);
+    }
+  )
 };
 
 const queryChain = (done) => {
